8825181d87d33d2799a04a3202bc50002fac724a
Varun Ramesh <varunramesh@live.com>
2018-02-05 19:24:14 -0800

    Added options to list-timecodes.

--- list-timecodes.py
+++ list-timecodes.py
@@ -18,33 +18,44 @@ except OSError as exc:
 
 VIDEO_EXTENSIONS = set([".mp4"])
 
-for root, dirs, files in os.walk('.'):
-    for filename in files:
-        if os.path.splitext(filename)[1] not in VIDEO_EXTENSIONS:
-            continue
+parser = argparse.ArgumentParser(prog="list-timecodes",
+        description="Show timecodes and completion for videos.")
+parser.add_argument('files', metavar='N', nargs='*',
+            help='The video files or directories.', default=['.'])
+args = parser.parse_args()
 
-        filepath = os.path.join(root, filename)
+def print_completion(filepath):
+    # Calculate file sha
+    with open(filepath, 'rb') as f:
+        h = hashlib.sha1()
+        h.update(f.read())
+        shasum = h.hexdigest()
 
-        # Calculate file sha
-        with open(filepath, 'rb') as f:
-            h = hashlib.sha1()
-            h.update(f.read())
-            shasum = h.hexdigest()
+    timecode = 0.0
+    timecode_file = os.path.join(TIMECODES_DIR, shasum + ".timecode")
+    if os.path.isfile(timecode_file):
+        with open(timecode_file, "r") as f:
+            timecode = float(f.read())
 
-        timecode = 0.0
-        timecode_file = os.path.join(TIMECODES_DIR, shasum + ".timecode")
-        if os.path.isfile(timecode_file):
-            with open(timecode_file, "r") as f:
-                timecode = float(f.read())
+    duration = float(FFProbe(filepath).video[0].duration)
+    completion = timecode / duration
 
-        duration = float(FFProbe(filepath).video[0].duration)
-        completion = timecode / duration
+    if timecode > duration - 10:
+        completion = 1
 
-        if timecode > duration - 10:
-            completion = 1
+    out_line = "{}\t{:0.1f}%\t{:0.2f}/{:0.2f}".format(filepath, completion*100, timecode, duration)
+    if completion == 1:
+        print(Fore.GREEN + out_line + Style.RESET_ALL)
+    else:
+        print(Fore.RED + out_line + Style.RESET_ALL)
 
-        out_line = "{}\t{:0.1f}%\t{:0.2f}/{:0.2f}".format(filepath, completion*100, timecode, duration)
-        if completion == 1:
-            print(Fore.GREEN + out_line + Style.RESET_ALL)
-        else:
-            print(Fore.RED + out_line + Style.RESET_ALL)
+for file in args.files:
+    if os.path.isfile(file):
+        print_completion(file)
+    elif os.path.isdir(file):
+        for root, dirs, files in os.walk(file):
+            for filename in files:
+                if os.path.splitext(filename)[1] not in VIDEO_EXTENSIONS:
+                    continue
+                filepath = os.path.join(root, filename)
+                print_completion(filepath)
